36604e1293729d55053eae06094f37c7f5fbacd9
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-05-31 22:46:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var a = []
-  
+
   for (let i = 0; i < musicians.length; i++) {
     a[i] = `I am ${musicians[i]} plays ${instruments[i]}.`
   }
