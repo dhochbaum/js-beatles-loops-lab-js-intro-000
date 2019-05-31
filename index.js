8fb246834f90e1d73c705d06cfd3fb98bbfc62fd
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-05-31 22:46:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var a = []
+  
+  for (let i = 0; i < musicians.length; i++) {
+    a[i] = `I am ${musicians[i]} plays ${instruments[i]}.`
+  }
+  return a
+}
